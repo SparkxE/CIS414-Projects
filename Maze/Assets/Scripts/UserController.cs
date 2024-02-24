ad7fb19fc5628a64ac2e099d7b03a8e7a731ded8
saidions01 <101123304+saidions01@users.noreply.github.com>
2024-02-24 14:46:44 -0600

    Observer Pattern
    - every time the player hits the wall the camera shakes (when we add the enemies we can add this behavior when the player gets shot )
    - when we press on take damage button, the health decrease

---
+++ Maze/Assets/Scripts/UserController.cs
@@ -0,0 +1,84 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UserController : Subject
+{
+    private float userHealth = 100;
+    private bool isWalking = false;
+    private HUDController hudController;
+    private CameraController cameraController;
+    private bool isHit = false;
+
+    public float UserHealth
+    {
+        get { return userHealth; } 
+    }
+
+    public bool IsWalking
+    {
+        get { return isWalking; }
+    }
+
+    public bool IsHit
+    {
+        get { return isHit; }
+        set { isHit = value; }
+    }
+
+    private void Awake()
+    {
+        hudController = gameObject.AddComponent<HUDController>();
+        cameraController = FindObjectOfType<CameraController>();
+    }
+
+    private void Start()
+    {
+        StartWalking();
+    }
+
+    private void OnEnable()
+    {
+        if (hudController)
+        {
+            Attach(hudController);
+        }
+
+        if (cameraController)
+        {
+            Attach(cameraController);
+        }
+    }
+
+    private void OnDisable()
+    {
+        if (hudController)
+        {
+            Detach(hudController);
+        }
+
+        if (cameraController)
+        {
+            Detach(cameraController);
+        }
+    }
+
+    private void StartWalking()
+    {
+        isWalking = true;
+        NotifyObservers();
+    }
+
+    public void TakeDamage(float amountOfDamage)
+    {
+
+        userHealth = userHealth - amountOfDamage;
+
+        NotifyObservers();
+
+        if (userHealth < 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
